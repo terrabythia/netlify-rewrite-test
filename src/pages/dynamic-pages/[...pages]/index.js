24ea4fe258dcc6c2d67bfac3b9cfd35d8b138fcf
Sander Bruggeman <sander.bruggeman@greenberry.nl>
2023-06-06 15:59:03 +0200

    append query string for extra test

--- src/pages/dynamic-pages/[...pages]/index.js
+++ src/pages/dynamic-pages/[...pages]/index.js
@@ -3,14 +3,15 @@ import Link from "next/link";
 const paramOptions = ['one', 'two', 'three'];
 
 const Page = ({ query, params }) => {
+  const queryString = new URLSearchParams(query || {}).toString();
   const currentParam = params.pages[params.pages.length - 1];
   const nextParam = paramOptions[(paramOptions.indexOf(currentParam) + 1) % paramOptions.length];
   return (
     <div>
       <h1>Page</h1>
       <p>I am rendered server side (SSR)</p>
-      <Link href={`/render/ssr/${nextParam}`}>
-        Go to /render/ssr/{nextParam}
+      <Link href={`/render/ssr/${nextParam}?${queryString}`}>
+        Go to /render/ssr/{nextParam}?{queryString}
       </Link>
       <pre>{JSON.stringify({
         query,
